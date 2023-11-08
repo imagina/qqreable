4698c7417ba994e118e44149499e0628da4461a7
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2023-11-08 12:51:31 -0500

    - added donwload - added modal - added fields

--- _crud/qrs.vue
+++ _crud/qrs.vue
@@ -1,9 +1,49 @@
-<template></template>
+<template>
+  <master-modal
+    v-model="modal.show"
+    :title="modal.item.title"
+    width="600px"
+  >
+    <div class="row">
+      <div class="col-12 text-center">
+        <q-card class="row q-pa-md">
+          <div class="col-5">
+            <div style="width: 160px; height: 160px">
+              <avatar-image
+                size="160px"
+                :src="modal.item.base64"
+                style="image-rendering: pixelated;"
+              />
+            </div>
+          </div>
+          <div class="col-7">
+            <p class="text-left">
+              {{ modal.item.content}}
+            </p>
+          </div>
+        </q-card>
+        <q-btn
+          :label="$tr('isite.cms.label.download')"
+          @click="downloadFile(modal.item)"
+          icon="fa-light fa-download"
+          no-caps
+          rounded
+          color="primary"
+          class="q-my-md"
+        />
+      </div>
+    </div>
+  </master-modal>
+</template>
 <script>
 export default {
   data() {
     return {
-      crudId: this.$uid()
+      crudId: this.$uid(),
+      modal: {
+        show: false,
+        item: false
+      }
     }
   },
   computed: {
@@ -30,30 +70,47 @@ export default {
               sortable: true,
             },
             {
-              name: 'zone', label: this.$tr('isite.cms.form.zone'),
-              field: 'zone',
-              align: 'left',
-              sortable: true,
-            },
-            {
               name: 'content', label: this.$tr('isite.cms.form.content'),
               field: 'content',
               align: 'left',
               sortable: true,
             },
             {
-              name: 'createdAt', label: this.$tr('isite.cms.form.createdAt'), field: 'createdAt',
-              format: val => val ? this.$trd(val) : '-',
-              align: 'left', sortable: true
+              name: 'entity_type', label: this.$tr('isite.cms.form.entity_type'),
+              field: 'entity_type',
+              align: 'left',
+              sortable: true,
+            },
+            {
+              name: 'entity_id', label: this.$tr('isite.cms.form.entity_id'),
+              field: 'entity_id',
+              align: 'left',
+              sortable: true,
+            },
+            {
+              name: 'zone', label: this.$tr('isite.cms.form.zone'),
+              field: 'zone',
+              align: 'left',
+              sortable: true,
             },
             {
               name: 'actions', label: this.$tr('isite.cms.form.actions'), align: 'center'
             },
           ],
+          actions: [
+            {
+              name: 'viewCode',
+              icon: 'fas fa-eye',
+              color: 'info',
+              tooltip: this.$tr('isite.cms.label.view'),
+              action: (item) => {
+                this.modal.item = item
+                this.modal.show = true
+              }
+            }
+          ]
         },
-        update: {
-          title: this.$tr('iqreable.cms.updatePost'),
-        },
+        update: false,
         delete: true,
         formLeft: {          
           id: {value: ''},
@@ -86,6 +143,14 @@ export default {
               ],
             },
           },
+          content: {
+            value: '',
+            type: 'input',
+            isTranslatable: false,
+            props: {
+              label: `${this.$tr('isite.cms.form.content')}*`,
+            },
+          },
           zone: {
             value: 'mainqr',
             type: 'input',
@@ -95,16 +160,9 @@ export default {
               rules: [
                 val => !!val || this.$tr('isite.cms.message.fieldRequired')
               ],
+              vIf: false,
             },
-          },
-          content: {
-            value: '',
-            type: 'input',
-            isTranslatable: false,
-            props: {
-              label: `${this.$tr('isite.cms.form.content')}*`,
-            },
-          },
+          }
         },
       }
     },
@@ -113,5 +171,20 @@ export default {
       return this.$store.state.qcrudComponent.component[this.crudId] || {}
     }
   },
+  methods: {
+    downloadFile(file) {
+      const fileUrl = file.base64;
+      const fileName = file.title;
+      const downloadLink = document.createElement('a');
+      downloadLink.href = fileUrl;
+      downloadLink.download = fileName;
+      downloadLink.target = '_blank';
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      setTimeout(() => {
+        document.body.removeChild(downloadLink);
+      }, 100);
+    },
+  }
 }
 </script>
