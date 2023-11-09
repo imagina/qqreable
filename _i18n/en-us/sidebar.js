19637e51bfe13ea28d8ef1c9b3db3213fef5ea52
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2023-11-09 15:04:53 -0500

    - removed unused translations

--- _i18n/en-us/sidebar.js
+++
@@ -1,6 +0,0 @@
-export default {
-  adminGroup : 'Blog',
-  adminPosts : 'Blog Posts',
-  adminCategories : 'Blog Categories',
-  adminPostsInternal : 'View Blog Posts',
-}
--- _i18n/es/sidebar.js
+++
@@ -1,6 +0,0 @@
-export default {
-  adminGroup: 'Blog',
-  adminPosts: 'Entradas de Blog',
-  adminCategories: 'Categorias de Blog',
-  adminPostsInternal: 'Ver entradas de Blog',
-}
