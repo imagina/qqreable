0009aea2be7b7538099cc93d46ec152afefe403e
Noith <nmontealegre@imaginacolombia.com>
2024-02-10 11:42:22 -0500

    Fixed: change import from @imagina/qsite/_layouts to layouts

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -5,13 +5,13 @@ export default {
     authenticated: true,
     path: '/qreable/qrs',
     name: 'iqreable.qrs',
-    crud : import('@imagina/qqreable/_crud/qrs'),
-    page: () => import('@imagina/qcrud/_pages/admin/crudPage'),
-    layout: () => import('@imagina/qsite/_layouts/master.vue'),
+    crud : import('modules/qqreable/_crud/qrs'),
+    page: () => import('modules/qcrud/_pages/admin/crudPage'),
+    layout: () => import('layouts/master.vue'),
     title: 'qrs',
     icon: 'fa-light fa-qrcode',
     subHeader: {
       refresh: true,
     }
-  }  
+  }
 }
